8f5c69ca431a2374190cfbddc2db7e46e9645bf4
Ivanovich0705 <ivantenis1@icloud.com>
2022-12-15 21:54:04 -0500

    some changes for three js

--- src/components/voxel.vue
+++ src/components/voxel.vue
@@ -18,15 +18,15 @@ export default {
       console.log(randomObject)
       switch (randomObject) {
         case 0:
-          this.glbPath = '/keyboard.glb'
+          this.glbPath = 'keyboard.glb'
           this.objectFov = 5
           break;
         case 1:
-          this.glbPath = '/oldpc.glb'
+          this.glbPath = 'oldpc.glb'
           this.objectFov = 2.5
           break;
         case 2:
-          this.glbPath = '/spaceship.glb'
+          this.glbPath = 'spaceship.glb'
           this.objectFov = 30
           break;
       }
